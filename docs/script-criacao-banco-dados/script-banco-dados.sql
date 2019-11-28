24a7014211b9167eacfe586c95adf773103540f0
Lucas Neves <lucas54neves@gmail.com>
2019-11-28 11:06:05 -0300

    #2 Atualização do banco de dados

--- docs/script-criacao-banco-dados/script-banco-dados.sql
+++ docs/script-criacao-banco-dados/script-banco-dados.sql
@@ -18,13 +18,12 @@ USE `imobiliaria` ;
 -- Table `imobiliaria`.`Proprietario`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `imobiliaria`.`Proprietario` (
-  `id` INT NOT NULL,
   `cpf` VARCHAR(11) NOT NULL,
   `nome` VARCHAR(100) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `nascimento` DATE NOT NULL,
   `senha` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`cpf`))
 ENGINE = InnoDB;
 
 
@@ -38,13 +37,7 @@ CREATE TABLE IF NOT EXISTS `imobiliaria`.`Imovel` (
   `bairro` VARCHAR(45) NOT NULL,
   `aluguel` DOUBLE NOT NULL,
   `idProprietario` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Imovel_Proprietario_idx` (`idProprietario` ASC),
-  CONSTRAINT `fk_Imovel_Proprietario`
-    FOREIGN KEY (`idProprietario`)
-    REFERENCES `imobiliaria`.`Proprietario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -52,14 +45,13 @@ ENGINE = InnoDB;
 -- Table `imobiliaria`.`Cliente`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `imobiliaria`.`Cliente` (
-  `id` INT NOT NULL,
   `cpf` VARCHAR(11) NOT NULL,
   `nome` VARCHAR(100) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `renda` DOUBLE NOT NULL,
   `nascimento` DATE NOT NULL,
   `senha` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`cpf`))
 ENGINE = InnoDB;
 
 
@@ -67,12 +59,11 @@ ENGINE = InnoDB;
 -- Table `imobiliaria`.`Funcionario`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `imobiliaria`.`Funcionario` (
-  `id` INT NOT NULL,
   `cpf` VARCHAR(11) NOT NULL,
   `nome` VARCHAR(100) NOT NULL,
   `nascimento` DATE NOT NULL,
   `senha` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`cpf`))
 ENGINE = InnoDB;
 
 
@@ -82,33 +73,33 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `imobiliaria`.`Contrato` (
   `idContrato` INT NOT NULL,
   `aluguelContrato` DOUBLE NULL,
-  `idProprietario` INT NOT NULL,
-  `idCliente` INT NOT NULL,
   `idImovel` INT NOT NULL,
-  `idFuncionario` INT NOT NULL,
+  `cpfProprietario` VARCHAR(11) NOT NULL,
+  `cpfFuncionario` VARCHAR(11) NOT NULL,
+  `cpfCliente` VARCHAR(11) NOT NULL,
   PRIMARY KEY (`idContrato`),
-  INDEX `fk_Contrato_Proprietario1_idx` (`idProprietario` ASC),
-  INDEX `fk_Contrato_Cliente1_idx` (`idCliente` ASC),
   INDEX `fk_Contrato_Imovel1_idx` (`idImovel` ASC),
-  INDEX `fk_Contrato_Funcionario1_idx` (`idFuncionario` ASC),
-  CONSTRAINT `fk_Contrato_Proprietario1`
-    FOREIGN KEY (`idProprietario`)
-    REFERENCES `imobiliaria`.`Proprietario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Contrato_Cliente1`
-    FOREIGN KEY (`idCliente`)
-    REFERENCES `imobiliaria`.`Cliente` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  INDEX `fk_Contrato_Proprietario1_idx` (`cpfProprietario` ASC),
+  INDEX `fk_Contrato_Funcionario1_idx` (`cpfFuncionario` ASC),
+  INDEX `fk_Contrato_Cliente1_idx` (`cpfCliente` ASC),
   CONSTRAINT `fk_Contrato_Imovel1`
     FOREIGN KEY (`idImovel`)
     REFERENCES `imobiliaria`.`Imovel` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Contrato_Proprietario1`
+    FOREIGN KEY (`cpfProprietario`)
+    REFERENCES `imobiliaria`.`Proprietario` (`cpf`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_Contrato_Funcionario1`
-    FOREIGN KEY (`idFuncionario`)
-    REFERENCES `imobiliaria`.`Funcionario` (`id`)
+    FOREIGN KEY (`cpfFuncionario`)
+    REFERENCES `imobiliaria`.`Funcionario` (`cpf`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Contrato_Cliente1`
+    FOREIGN KEY (`cpfCliente`)
+    REFERENCES `imobiliaria`.`Cliente` (`cpf`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
